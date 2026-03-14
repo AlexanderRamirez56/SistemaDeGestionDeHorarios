f524a3397babb0878c8acb289fe8651c33b6cdcc
AlexanderRamirez56 <alexanderrjimenez21@gmail.com>
2026-03-13 23:22:44 -0400

    Mejora visual

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,29 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeHorarios.Data;
 using SistemaDeHorarios.Models;
+using System.Diagnostics;
 
 namespace SistemaDeHorarios.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
+            ViewBag.Docentes = _context.Docentes.Count();
+            ViewBag.Asignaturas = _context.Asignaturas.Count();
+            ViewBag.Aulas = _context.Aulas.Count();
+            ViewBag.Grupos = _context.Grupos.Count();
+            ViewBag.Horarios = _context.Horarios.Count();
 
-        public IActionResult Privacy()
-        {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
